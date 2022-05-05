0ec40cae426d3290775e99f4475c565dba7170f4
styller1 <pcraveirodeandrade@gmail.com>
2022-05-05 17:47:46 -0300

    primeira aula de javascript

---
+++ Js/Main.js
@@ -0,0 +1,10 @@
+//o java identifica se é texto ou numero pela forma como é escrito"
+var nome = "Pablo Craveiro";
+var idade = 26;
+var idade2 = 10;
+var frase= "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade+idade2);
+//console.log(frase.replace("Japão",  "brasil"));
